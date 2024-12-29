e78c6927fc8bdbec39e4ea9690af00e81ca4a751
wole abraham <wole1702@gmail.com>
2024-12-29 11:00:22 -0800

    refactor keypair generation to use environment variables for keys

--- generate_keypair.ts
+++ generate_keypair.ts
@@ -1,6 +1,12 @@
 import { Keypair } from "@solana/web3.js";
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const keypair = Keypair.generate();
 
-console.log(`The Public key is: `, keypair.publicKey.toBase58());
-console.log(`The secret key is: `, keypair.secretKey);
+// console.log(`The Public key is: `, keypair.publicKey.toBase58());
+// console.log(`The secret key is: `, keypair.secretKey);
+
+console.log(process.env.PUBLIC_KEY)
+console.log(process.env.SECRET_KEY)
